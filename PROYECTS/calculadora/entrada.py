0ab1c21164bd2292bed1656c271e78aaf4791249
dagaser <danieldag-01@hotmail.com>
2025-07-25 00:07:04 -0500

    Eliminada carpeta de proyecto anterior

--- PROYECTS/calculadora/entrada.py
+++
@@ -1,7 +0,0 @@
-def leer_numero(mensaje):
-    while True:
-        try:
-            numero = float(input(mensaje))
-            return numero
-        except ValueError:
-            print("Entrada inválida. Por favor, ingrese un número válido.")
